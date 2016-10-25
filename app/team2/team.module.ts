306e05bdb3e5fa53f4d8a4ebd5fb2fd8a66ef0f7
Jen Bourey <jennifer.bourey@gmail.com>
2016-10-25 09:46:34 +0200

    Fixing typo

--- app/team2/team.module.ts
+++ app/team2/team.module.ts
@@ -6,7 +6,7 @@ import { TeamServiceModule } from '../common/team/team.service';
 let ROUTES = [
   { path: 'teams', children: [
     { path: '', loadChildren: 'app/team2/team_list.module' },
-    { path: ':teamId', loadChildren: 'app2/team2/team_detail.module' }
+    { path: ':teamId', loadChildren: 'app/team2/team_detail.module' }
   ]}
 ];
 
