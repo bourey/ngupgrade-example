a6bac1b5f673f837025f2dec0d7eaf75eada0e49
Jen Bourey <jennifer.bourey@gmail.com>
2016-10-26 12:05:04 +0200

    Splitting out ng2 pre and post lazy loading

--- app/team2/team.module.ts
+++ app/team2/team.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { TeamServiceModule } from '../common/team/team.service';
+import { TeamComponentModule } from './team_detail.module';
+import { TeamsComponentModule } from './team_list.module';
 
 let ROUTES = [
   { path: 'teams', children: [
-    { path: '', loadChildren: 'app/team2/team_list.module#TeamsComponentModule' },
-    { path: ':teamId', loadChildren: 'app/team2/team_detail.module#TeamComponentModule' }
+    { path: '', loadChildren: () => TeamsComponentModule },
+    { path: ':teamId', loadChildren: () => TeamComponentModule }
   ]}
 ];
 
 
 @NgModule({
   imports: [
+    TeamComponentModule,
+    TeamsComponentModule,
     TeamServiceModule,
     RouterModule.forChild(ROUTES)
   ],
